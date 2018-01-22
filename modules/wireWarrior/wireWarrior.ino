0cfdc1247a858dbfea6ee2f933aa6eae078011d2
Charles Paulet <charles.paulet@epitech.eu>
2018-01-22 23:44:34 +0100

    Fix wireWarrior and add header on modules

--- modules/wireWarrior/wireWarrior.ino
+++ modules/wireWarrior/wireWarrior.ino
@@ -1,23 +1,37 @@
+/****************************************
+ * wireWarrior
+ *
+ * Charles Paulet
+ * Nicolas-Emmanuel Robert
+ *
+ * User need to connect 3 wires correctly
+ * to defuse the module. wire's pins are
+ * defined in wires[]. The patterns that
+ * must match are described in patterns[]
+ * and each correspond to a difficulty.
+ ***************************************/
+
 #include <ktne_core.h>
 
 #define WIRES (3)
 
 int wires[] = {4, 5, 6};
 
-bool patterns[MAX_DIFFICULTY][3] = {{HIGH, LOW, HIGH},
+bool patterns[MAX_DIFFICULTY][WIRES] = {{HIGH, LOW, HIGH},
                                         {LOW, HIGH, LOW},
                                         {LOW, LOW, LOW}};
 
 void setupWires(void)
 {
-  for (int i = wires[0]; i < sizeof(wires) + wires[0]; ++i)
+  for (int i = wires[0]; i < WIRES + wires[0]; ++i)
     pinMode(i, INPUT);
 }
 
 bool win(uint8_t difficulty)
 {
-  for (int i = wires[0]; i < sizeof(wires) + wires[0]; ++i)
+  for (int i = wires[0]; i < WIRES + wires[0]; ++i)
   {
+    Serial.println(patterns[difficulty - 1][i - 4]);
     if (digitalRead(i) != patterns[difficulty - 1][i - 4])
       return false;
   }
--- templates/slave/slave.ino
+++ templates/slave/slave.ino
@@ -1,3 +1,15 @@
+/****************************************
+ * example slave
+ *
+ * Charles Paulet
+ * Théophile Champion
+ *
+ * This is a slave example.
+ * User need to press button to defuse
+ * module (by calling defuseModule()
+ * from ktne_core).
+****************************************/
+
 #include <ktne_core.h>
 
 #define BUTTON (4)
