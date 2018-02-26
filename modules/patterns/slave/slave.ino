40609f8838efdf2e39e9baf18416055104edf59c
ChampiB <theophile.champion@epitech.eu>
2018-02-26 16:29:07 +0100

    Suppress define PATTERNS and replace it by NB_IMGS + Change define NB_IMGS from static value to dynamic

--- modules/patterns/slave/slave.ino
+++ modules/patterns/slave/slave.ino
@@ -24,8 +24,8 @@
 #include <TFT.h>
 #include <ktne_core.h>
 
-#define PIN_DIFFICULTY_0 (A0) /* interrupt to sync this module difficulty */
-#define PIN_DIFFICULTY_1 (A1) /* interrupt to sync this module difficulty */
+#define PIN_DIFFICULTY_0 (A0) /* sync the module difficulty */
+#define PIN_DIFFICULTY_1 (A1) /* sync the module difficulty */
 #define PIN_PENALITY   (6)
 #define PIN_DEFUSAL    (7)
 
@@ -33,16 +33,20 @@
 #define PIN_BTN_MIDDLE (3)
 #define PIN_BTN_RIGHT  (5)
 
-#define PATTERNS       (3)
 #define IMG_DIMENSIONS (48)
 #define IMG_MARGIN     (4)
 
 static uint16_t slave_difficulty = difficulty;
-static uint8_t btns[PATTERNS] = { PIN_BTN_LEFT, PIN_BTN_MIDDLE, PIN_BTN_RIGHT };
+
+static uint8_t const btns[] = { PIN_BTN_LEFT, PIN_BTN_MIDDLE, PIN_BTN_RIGHT };
+static char const * const filenames[] = { "cat.bmp", "logo.bmp", "vim.bmp" };
+static PImage images[] = { {}, {}, {} };
+
+#define NB_IMGS (sizeof(images) / sizeof(images[0]))
 
 typedef struct
 {
-  uint8_t const id[PATTERNS];
+  uint8_t const id[NB_IMGS];
   uint8_t const solution;
 } t_pattern;
 
@@ -52,15 +56,10 @@ static t_pattern const patterns[MAX_DIFFICULTY] = {
   {{ 2, 2, 0 }, PIN_BTN_RIGHT}
 };
 
-#define NB_IMGS (3)
-
-static char const * const filenames[] = { "cat.bmp", "logo.bmp", "vim.bmp", "sample.bmp", "hexagon.bmp", "vim.bmp", "w2d.bmp", "resitor.bmp", "jeremy.bmp", "epitech.bmp" };
-static PImage images[NB_IMGS] = { {}, {}, {} };
-
-#define sd_cs  (4)
-#define lcd_cs (10)
-#define dc     (9)
-#define rst    (8)
+#define sd_cs   (4)
+#define lcd_cs  (10)
+#define dc      (9)
+#define rst     (8)
 
 // screen
 TFT TFTscreen = TFT(lcd_cs, dc, rst);
@@ -99,7 +98,7 @@ void printImages(void)
 {
   uint8_t y = TFTscreen.height() / 2 - IMG_DIMENSIONS / 2;
  
-  for (size_t i = 0 ; i < PATTERNS ; ++i)
+  for (size_t i = 0 ; i < NB_IMGS ; ++i)
     TFTscreen.image(images[patterns[slave_difficulty - 1].id[i]], IMG_MARGIN + (IMG_DIMENSIONS + IMG_MARGIN) * i, y);
 }
 
@@ -115,13 +114,6 @@ void setup()
   TFTscreen.background(0, 0, 0);
   loadImages();
   printImages();
-}
-
-void sendSignal(int pin)
-{
-  digitalWrite(pin, HIGH);
-  delay(100);
-  digitalWrite(pin, LOW);
 }
 
 void sendSignal(int pin)
